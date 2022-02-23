625d476f441fa6ef01a46fa26b23a87281b6405f
kileader <60195493+kileader@users.noreply.github.com>
2022-02-23 15:01:00 -0500

    Add problem as comment

--- week_2_21/Decimate.java
+++ week_2_21/Decimate.java
@@ -2,6 +2,13 @@ package week_2_21;
 
 import java.util.ArrayList;
 
+/*
+ * This classic problem dates back to Roman times. There are 41 soldiers arranged in a circle. 
+ * Every third soldier is to be killed by their captors, continuing around the circle until 
+ * only one soldier remains. He is to be freed. Assuming you would like to stay alive, at what position in the circle would you stand?
+ * Generalize this problem by creating a function that accepts the number of soldiers n and the 
+ * interval at which they are killed i, and returns the position of the fortunate survivor.
+ */
 public class Decimate {
 
     public static void main(String[] args) {
